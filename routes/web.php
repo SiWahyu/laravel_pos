e364ec1aae9f5403d1161fd4408e31664365f149
SiWahyu <fwahyu839@gmail.com>
2024-06-26 18:41:03 +0700

    membuat halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\DashboardController;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
